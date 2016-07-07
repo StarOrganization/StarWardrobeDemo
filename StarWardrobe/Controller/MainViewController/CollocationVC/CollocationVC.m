54a1e6891c4867f1f24d852275ba1e3447179cb1
XvYaChao <316332096@qq.com>
2016-07-07 14:16:43 +0800

    Xyc
    明星衣橱

---
+++ StarWardrobe/Controller/MainViewController/CollocationVC/CollocationVC.m
@@ -0,0 +1,39 @@
+//
+//  CollocationVC.m
+//  StarWardrobe
+//
+//  Created by Mac on 16/7/4.
+//  Copyright © 2016年 Xyc. All rights reserved.
+//
+
+#import "CollocationVC.h"
+
+@interface CollocationVC ()
+
+@end
+
+@implementation CollocationVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    self.navigationController.navigationBar.backgroundColor = [UIColor greenColor];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
